e6c33531b724eb11f7653763ef4ffae42604aa36
Wallace Gomes <wallacemesqti@gmail.com>
2026-06-01 09:38:45 -0300

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
-const bodyParser = require('body-parser')
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./src/data/database.json')
-const publicRoutes = require('./routes/publicRoutes')
-const authenticationMiddleware = require('./middleware/authenticationMiddleware')
+const bodyParser = require("body-parser");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./src/data/database.json");
+const publicRoutes = require("./routes/publicRoutes");
+const authenticationMiddleware = require("./middleware/authenticationMiddleware");
 
-
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 server.use(jsonServer.defaults());
 
-server.use('/public', publicRoutes) //rotas na porta 8000
+server.use("/public", publicRoutes); //rotas na porta 8000
 
+server.use(
+  /^(?!\/(public|livros|autores|categorias)).*$/,
+  authenticationMiddleware,
+);
 
-server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
-
-server.use(router) //rotas na prota 3000
+server.use(router); //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
-})
+  console.log("Boas vindas a api do allbooks");
+  console.log("API disponível atraves da url http://localhost:8000");
+});
